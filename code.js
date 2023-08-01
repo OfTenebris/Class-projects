7f3a995a3ee38a71b5bfc1dc05f3f35882248a9b
OfTenebris <140268873+OfTenebris@users.noreply.github.com>
2023-07-31 23:59:04 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,6 @@
+//Allows the button to function, displaying the nextr page in a separate window
+document.getElementById("openButton").addEventListener("click", function() 
+{
+var newDocumentURL = "C:/Users/theke/OneDrive/Documents/Programming/Choice.html";
+window.open(newDocumentURL, "_blank"); 
+});
